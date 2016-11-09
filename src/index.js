354cbf7b27b48e9817fdda8fef3fe6badef07c80
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-11-09 13:54:38 +0000

    added json parse and api

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// var news = new XMLHttpRequest();
+// news.open('GET', "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body", true);
+// news.send();
+// news.addEventListener("readystatechange", processRequest, false);
+// news.onreadystatechange = processRequest;
+// function processRequest(e) {
+//     if (news.readyState == 4 && news.status == 200) {
+//         var response = JSON.parse(news.responseText);
+//     }
+//     console.log(response);
+//   }
