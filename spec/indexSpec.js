4dacf7c3b752f0bba370b7f46ff8a18e81b1fd59
fbell123 <fbell.pro@gmail.com>
2016-11-11 09:54:24 +0000

    third test added to check full article link works

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,21 +1,23 @@
 
 
-  ('Shows all articles', function() {
-    var site = window.location;
-    document.getElementById('all_articles').click();
-    contain.toContain(site, 'file:///Users/frankiebell/Makers/Projects/week-7/news-app/views/index.html#');
-  }());
+('Shows all articles', function() {
+  var site = window.location;
+  document.getElementById('all_articles').click();
+  contain.toContain(site, 'file:///Users/frankiebell/Makers/Projects/week-7/news-app/views/index.html#');
+}());
 
-  ('Shows single article', function() {
-        article = document.getElementById ("article");
-            article.addEventListener ('DOMSubtreeModified', function(){
-              frankie.haveContent('the')},
-               true);
-    }());
+('Shows single article', function() {
+  article = document.getElementById ("article");
+  article.addEventListener ('DOMSubtreeModified',
+  function(){ frankie.haveContent('the')},
+  true);
+}());
 
 
-  // ('Shows full article', function() {
-  //   var site = window.location.href;
-  //   document.getElementById('link').click();
-  //   expect.toContain(site, 'theguardian');
-  // }())
+('Shows full article', function() {
+  article = document.getElementById ("article");
+  document.getElementById('link').click();
+  article.addEventListener ('DOMSubtreeModified',
+  function(){ frankie.haveContent('theguardian')},
+  true);
+}())
